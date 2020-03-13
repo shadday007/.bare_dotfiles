4a2e17b35126e5d5b2527fba80f383c1c601ff81
Jorge Luis Hernández <shadday007@gmail.com>
2020-03-13 08:26:57 -0400

    add install script

---
+++ dotfiles_install.sh
@@ -0,0 +1,15 @@
+git clone --bare https://https://github.com/shadday007/.dotfiles.git $HOME/.dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
