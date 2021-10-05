1ee26cfaed0b68dbc7b5702af0eae999fece47ef
Jorge Luis Hernández <shadday007@gmail.com>
2021-10-05 14:00:52 -0400

    set wildmenu and add some blank spaces to README

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+~~~ sh
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/shadday007/.dotfiles.git tmpdotfiles
 rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+
 rm -r tmpdotfiles
 
 curl -Lks https://raw.githubusercontent.com/shadday007/.dotfiles/master/dotfiles_install.sh | /bin/bash
+~~~
--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,10 +1,15 @@
 git clone --bare https://github.com/shadday007/.dotfiles.git $HOME/.dotfiles
+
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
+
 mkdir -p .dotfiles-backup
+
 config config status.showUntrackedFiles no
+
 config checkout
+
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
@@ -16,5 +21,6 @@ if [ $? = 0 ]; then
     # move regular files
     config checkout 2>&1 | egrep "^\s+.+" | awk {'print $1'} | xargs -I{} mv -f {} .dotfiles-backup/{}
 fi;
+
 config checkout
 
